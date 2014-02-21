42309ab47e6e2e3f438695b4ec5aa4bc090c5d00
anurse <andrew@andrewnurse.net>
2014-02-21 15:54:35 -0800

    Stashing initial search service work

---
+++ src/NuGet.Services.Search/Api/SomethingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NuGet.Services.Http;
+
+namespace NuGet.Services.Search.Api
+{
+    public class SomethingController : NuGetApiController
+    {
+        [Route("test")]
+        public string Test()
+        {
+            return "Search is up!";
+        }
+    }
+}
---
+++ src/NuGet.Services.Search/SearchService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using NuGet.Services.Http;
+using NuGet.Services.ServiceModel;
+
+namespace NuGet.Services.Search
+{
+    public class SearchService : NuGetApiService
+    {
+        public override PathString BasePath
+        {
+            get { return new PathString("search"); }
+        }
+
+        protected override Task OnRun()
+        {
+            // TODO: Indexer!
+        }
+    }
+}
