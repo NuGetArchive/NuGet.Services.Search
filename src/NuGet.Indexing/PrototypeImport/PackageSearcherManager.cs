0db78a6eae04811c9a1c3a74cba5e34550a058b2
anurse <andrew@andrewnurse.net>
2014-02-28 10:17:26 -0800

    Updating things so they build

---
+++ src/NuGet.Indexing/PrototypeImport/PackageSearcherManager.cs
@@ -0,0 +1,49 @@
+﻿using Lucene.Net.Search;
+using System;
+using System.Collections.Generic;
+
+namespace NuGet.Indexing
+{
+    public class PackageSearcherManager : SearcherManager
+    {
+        IDictionary<string, IDictionary<string, int>> _currentRankings;
+        DateTime _rankingsTimeStampUtc;
+        Rankings _rankings;
+
+        public PackageSearcherManager(Lucene.Net.Store.Directory directory, Rankings rankings)
+            : base(directory)
+        {
+            _rankings = rankings;
+            WarmTimeStampUtc = DateTime.UtcNow;
+        }
+
+        protected override void Warm(IndexSearcher searcher)
+        {
+            searcher.Search(new MatchAllDocsQuery(), 1);
+            WarmTimeStampUtc = DateTime.UtcNow;
+        }
+
+        public DateTime WarmTimeStampUtc
+        {
+            get;
+            private set;
+        }
+
+        public IDictionary<string, int> GetRankings(string context)
+        {
+            if (_currentRankings == null || (DateTime.Now - _rankingsTimeStampUtc) > TimeSpan.FromHours(24))
+            {
+                _currentRankings = _rankings.Load();
+                _rankingsTimeStampUtc = DateTime.UtcNow;
+            }
+
+            IDictionary<string, int> rankings;
+            if (_currentRankings.TryGetValue(context, out rankings))
+            {
+                return rankings;
+            }
+
+            return _currentRankings["Rank"];
+        }
+    }
+}
