b094af487cdfdd771545b24aa9ddb255f3f6cbce
anurse <andrew@andrewnurse.net>
2014-03-03 09:31:43 -0800

    Cleaned up crap code

---
+++ src/NuGet.Indexing/PackageRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuGet.Indexing
+{
+    public class PackageRegistration
+    {
+        public IEnumerable<string> Owners { get; set; }
+    }
+}
