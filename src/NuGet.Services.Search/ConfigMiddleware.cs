e1279f26cc435f3acda0542a6eddd65fadb10737
anurse <andrew@andrewnurse.net>
2014-03-03 17:30:40 -0800

    Integrated Owin Middleware from SearchApp.cs into Search Service

---
+++ src/NuGet.Services.Search/ConfigMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json.Linq;
+
+namespace NuGet.Services.Search
+{
+    public class ConfigMiddleware : SearchMiddleware
+    {
+        public ConfigMiddleware(OwinMiddleware next, string path, SearchMiddlewareConfiguration config) : base(next, path, config) { }
+
+        protected override Task Execute(IOwinContext context)
+        {
+            Trace.TraceInformation("Where");
+
+            JObject response = new JObject();
+
+            if (Config.UseStorage)
+            {
+                string accountName = Config.StorageAccount.Credentials.AccountName;
+                response.Add("AccountName", accountName);
+                response.Add("StorageContainer", Config.StorageContainer);
+
+                CloudBlobClient client = Config.StorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = client.GetContainerReference(Config.StorageContainer);
+                CloudBlockBlob blob = container.GetBlockBlobReference("segments.gen");
+
+                response.Add("IndexExists", blob.Exists());
+            }
+            else
+            {
+                response.Add("LocalIndexPath", Config.LocalIndexPath);
+            }
+
+            return WriteResponse(context, response.ToString());
+        }
+    }
+}
---
+++ src/NuGet.Services.Search/DiagMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Index;
+using Microsoft.Owin;
+using NuGet.Indexing;
+
+namespace NuGet.Services.Search
+{
+    public class DiagMiddleware : SearchMiddleware
+    {
+        public DiagMiddleware(OwinMiddleware next, string path, SearchMiddlewareConfiguration config) : base(next, path, config) { }
+
+        protected override Task Execute(IOwinContext context)
+        {
+            Trace.TraceInformation("Diag");
+
+            return WriteResponse(context, IndexAnalyzer.Analyze(GetSearcherManager()));
+        }
+    }
+}
