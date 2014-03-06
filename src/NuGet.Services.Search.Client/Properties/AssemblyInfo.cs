b44efc0ab9c4936c71c1ab22cb3b3d46c860d4d9
anurse <andrew@andrewnurse.net>
2014-03-06 13:53:22 -0800

    Started on client library

---
+++ src/NuGet.Services.Search.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Services.Search.Client")]
+[assembly: AssemblyDescription("Client library for the NuGet Search Service")]
