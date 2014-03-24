f28983c1b51b58ad5d9a2fb2472fc99d8642e27b
anurse <andrew@andrewnurse.net>
2014-03-24 14:33:08 -0700

    Cleaned up old build scripts

---
+++ src/IndexMaintenance/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Indexing.Maintenance")]
+[assembly: AssemblyDescription("NuGet Index Maintenance tool")]
+[assembly: InternalsVisibleTo("NuGet.Indexing.Facts")]
