e91a57c1f43b920a683ae4b6e4c0ab64c18efc48
johnataylor <johtaylo@microsoft.com>
2015-02-23 16:08:22 -0800

    move project to scr folder

--- src/NuGet.Indexing/MetadataImpl.cs
+++ src/NuGet.Indexing/MetadataImpl.cs
@@ -1,20 +1,15 @@
-﻿using Lucene.Net.Index;
-using Lucene.Net.Search;
+﻿using Lucene.Net.Search;
 using Microsoft.Owin;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NuGet.Indexing
 {
     public static class MetadataImpl
     {
-        public static void CheckAccess(IOwinContext context, string tenantId, NuGetSearcherManager searcherManager, string connectionString)
+        public static void Access(IOwinContext context, string tenantId, SecureSearcherManager searcherManager, string connectionString, int accessDuration)
         {
             searcherManager.MaybeReopen();
 
@@ -26,23 +21,30 @@ namespace NuGet.Indexing
 
                 string[] parts = relativeUri.Split('/');
 
-                string containerName = parts[parts.Length - 2];
-                string blobName = parts[parts.Length - 1];
-
-                CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
-                CloudBlobClient client = account.CreateCloudBlobClient();
-                CloudBlobContainer container = client.GetContainerReference(containerName);
-                CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
-
-                SharedAccessBlobPolicy sharedPolicy = new SharedAccessBlobPolicy()
+                if (parts.Length >= 3)
                 {
-                    SharedAccessExpiryTime = DateTime.UtcNow.AddSeconds(10),
-                    Permissions = SharedAccessBlobPermissions.Read
-                };
+                    string containerName = parts[parts.Length - 2];
+                    string blobName = parts[parts.Length - 1];
 
-                string sharedAccessSignature = blob.GetSharedAccessSignature(sharedPolicy);
+                    CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+                    CloudBlobClient client = account.CreateCloudBlobClient();
+                    CloudBlobContainer container = client.GetContainerReference(containerName);
+                    CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
 
-                context.Response.Redirect(blob.Uri + sharedAccessSignature);
+                    SharedAccessBlobPolicy sharedPolicy = new SharedAccessBlobPolicy()
+                    {
+                        SharedAccessExpiryTime = DateTime.UtcNow.AddSeconds(accessDuration),
+                        Permissions = SharedAccessBlobPermissions.Read
+                    };
+
+                    string sharedAccessSignature = blob.GetSharedAccessSignature(sharedPolicy);
+
+                    context.Response.Redirect(blob.Uri + sharedAccessSignature);
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
             }
             finally
             {
