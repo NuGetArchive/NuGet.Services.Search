abfebfe84b909328bbd403b564e3d1789b98f6f9
johnataylor <johtaylo@microsoft.com>
2015-02-24 17:23:32 -0800

    clean up dead code

--- src/IndexMaintenance/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NuGet.Indexing.Maintenance")]
-[assembly: AssemblyDescription("NuGet Index Maintenance tool")]
-[assembly: InternalsVisibleTo("NuGet.Indexing.Facts")]
