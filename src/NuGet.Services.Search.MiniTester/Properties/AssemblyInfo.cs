1f2e9efe52d7e43dfbad27ffaa4c087b393564ad
johnataylor <johtaylo@microsoft.com>
2015-02-25 16:05:59 -0800

    updated packages - clean out test project

--- src/NuGet.Services.Search.MiniTester/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NuGet.Services.Search.MiniTester")]
-[assembly: AssemblyDescription("Mini Owin-based Test host for search server")]
