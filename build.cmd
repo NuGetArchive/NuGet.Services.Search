f1975770307cc89b1706b8870cfc19348ad80eb1
Xavier Decoster <xavier.decoster@outlook.com>
2015-03-19 16:33:47 +0100

    new build.cmd

--- build.cmd
+++ build.cmd
@@ -1,1 +1,11 @@
-@msbuild build.msbuild %*
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+REM Package restore
+tools\nuget.exe restore NuGet.Services.Search.sln -OutputDirectory %cd%\packages -NonInteractive
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild NuGet.Services.Search.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
