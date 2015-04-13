576c26e64898393a03a8a4a7e8bf32fdb7f3872b
Xavier Decoster <xavier.decoster@outlook.com>
2015-04-13 14:45:39 +0200

    added assembly attributes required for packaging

--- src/NuGet.Services.Search.Client/Properties/AssemblyInfo.cs
+++ src/NuGet.Services.Search.Client/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("NuGet.Services.Search.Client")]
 [assembly: AssemblyDescription("Client library for the NuGet Search Service")]
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet Services")]
+[assembly: AssemblyCopyright("\x00a9 Outercurve Foundation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
