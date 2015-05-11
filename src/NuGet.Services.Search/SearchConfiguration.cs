eb1e3437214c492685a6a22698bdfa19dddfdc92
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-11 18:52:53 +0200

    Update license header on files.

--- src/NuGet.Services.Search/SearchConfiguration.cs
+++ src/NuGet.Services.Search/SearchConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace NuGet.Services.Search
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+namespace NuGet.Services.Search
 {
     public class SearchConfiguration
     {
